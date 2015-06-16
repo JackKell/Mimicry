77e642d8d9638490423066f4e61f4b939ad53f63
Jackkell <bronzeeagle1@hotmail.com>
2015-06-15 19:59:33 -0700

    Created Config Class

---
+++ app/src/main/java/com/github/jackkell/mimicryproject/Config/Config.java
@@ -0,0 +1,8 @@
+package com.github.jackkell.mimicryproject.Config;
+
+public class Config {
+    public final static String CONSUMER_KEY = "";
+    public final static String CONSUMER_KEY_SECRET = "";
+    public final static String ACCESS_TOKEN = "";
+    public final static String ACCESS_TOKEN_SECRET = "";
+}
--- app/src/main/java/com/github/jackkell/mimicryproject/TwitterTasks/SendTweetTask.java
+++ app/src/main/java/com/github/jackkell/mimicryproject/TwitterTasks/SendTweetTask.java
@@ -2,7 +2,7 @@ package com.github.jackkell.mimicryproject.TwitterTasks;
 
 import android.os.AsyncTask;
 
-import com.github.jackkell.mimicryproject.Config;
+import com.github.jackkell.mimicryproject.Config.Config;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -15,7 +15,7 @@ public class SendTweetTask extends AsyncTask<String, Void, Boolean> {
     @Override
     protected Boolean doInBackground(String... strings) {
         Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(Config.COSUMER_KEY, Config.CONSUMER_KEY_SECRET);
+        twitter.setOAuthConsumer(Config.CONSUMER_KEY, Config.CONSUMER_KEY_SECRET);
         twitter.setOAuthAccessToken(new AccessToken(Config.ACCESS_TOKEN, Config.ACCESS_TOKEN_SECRET));
 
         try {
