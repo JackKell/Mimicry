29859e724eca73286e6f8f799581066297f5e613
Jackkell <bronzeeagle1@hotmail.com>
2015-06-26 23:45:29 -0700

    Fixed code

--- app/src/main/java/com/github/jackkell/mimicryproject/Config/Config.java
+++ app/src/main/java/com/github/jackkell/mimicryproject/Config/Config.java
@@ -1,8 +1,8 @@
 package com.github.jackkell.mimicryproject.Config;
 
 public class Config {
-    public final static String CONSUMER_KEY = "DPl6tlEhUiYEvrEDbhNPS8xen";
-    public final static String CONSUMER_KEY_SECRET = "M3yyPqkb90JVhfIL2jH0c68lNjXNsxzyoZgUMB5xTE0NOg8LSu";
-    public final static String ACCESS_TOKEN = "3246462427-eQH0jE7gYWLiXpwuaLWoIc5irRbA874By47AbuR";
-    public final static String ACCESS_TOKEN_SECRET = "12JsGAx0z5UsFB2MamHCdFKnELSu2oY9Q9NsaH1p3rrCc";
+    public final static String CONSUMER_KEY = "";
+    public final static String CONSUMER_KEY_SECRET = "";
+    public final static String ACCESS_TOKEN = "";
+    public final static String ACCESS_TOKEN_SECRET = "";
 }
